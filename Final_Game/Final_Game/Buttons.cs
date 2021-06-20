e698f72e406451b26480b4f7592f7cec7295573c
Maurits Dijkman <78701018+TheRealMaurits@users.noreply.github.com>
2021-06-20 22:08:25 +0200

    Add files via upload

---
+++ Final_Game/Final_Game/Buttons.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using GXPEngine;
+
+public class Buttons : AnimationSprite
+{
+    public Buttons(Vec2 position, int frameNumber) : base("Buttons_Tile.png", 4, 2)
+    {
+        SetXY(position.x, position.y);
+        SetOrigin(width /2, height / 2);
+        SetFrame(frameNumber);
+    }
+}
---
+++ Final_Game/Final_Game/ScoreBall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using GXPEngine;
+
+public class ScoreBall : Sprite
+{
+    MyGame _myGame = null;
+
+    Ball _scoreball;
+
+    Sound _scoreSound;
+
+    public ScoreBall(Vec2 position) : base("Score_Ball.png")
+    {
+        _myGame = (MyGame)game;
+
+        _scoreSound = new Sound("Score_Sound.wav", false, true);
+
+        SetOrigin(width / 2, height / 2);
+        SetXY(position.x, position.y);
+        _myGame.AddChild(this);
+
+        // Creates a new Ball and calls it _scoreball
+        _scoreball = new Ball(255, 255, 255, 28, position, new Vec2(0, 0), false, false);
+        _scoreball._scoreball = this;
+
+        // Adds the _scoreball to mygame
+        _myGame._ball.Add(_scoreball);
+        _myGame.AddChild(_scoreball);
+    }
+
+    public void SoundEffect()
+    {
+        _scoreSound.Play();
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
+    //                                                                      DeleteScoreBall()                                                                    //
+    //-----------------------------------------------------------------------------------------------------------------------------------------------------------//
+    public void DeleteScoreBall()
+    {
+        _myGame._ball.Remove(_scoreball);
+        _scoreball.Destroy();
+
+        Destroy();
+    }
+}
